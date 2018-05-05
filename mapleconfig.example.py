6141ecc12520c85bd3395647eff4f1c2c3333f78
ari <sndaritsune@gmail.com>
2018-05-05 12:53:59 -0300

    draftadd

--- mapleconfig.example.py
+++ mapleconfig.example.py
@@ -1,8 +1,10 @@
 def get_token():
     return "ya dang token goes here ;333"
 
+
 def get_mainchannel_id():
     return "ya main channel goes here!!!"
+
 
 def get_debug_whitelist():
     return ["list of discord ids with debug permission here!!!"]
